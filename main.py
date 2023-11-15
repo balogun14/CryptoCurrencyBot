3c2143b6e32da05e6c3477063140ff7503bcda87
balogun14 <awwalbalogun06@gmail.com>
2023-11-15 19:21:31 +0100

    Add a little chat moderation that greets new users

--- main.py
+++ main.py
@@ -11,10 +11,25 @@ bot.
 """
 import logging
 import os
-from turtle import update
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from typing import Optional, Tuple
+from telegram import (
+    Bot,
+    ChatMember,
+    ChatMemberUpdated,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    MessageEntity,
+    Update,
+)
 from telegram.constants import ChatAction
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    filters,
+    MessageHandler,
+    ChatMemberHandler,
+)
 from scrapper import news_scrapper, price_list_scrapper
 from functions import image_handler
 
@@ -34,8 +49,8 @@ commands = {
     "/start": "starts the bot",
     "/news": "Gets the current news from the stock market",
     "/price": "Gets the price of the stock asked",
-    "/rules": "reminds you of the channel rules",
-    "/delete": "delete the message that violates group rules",
+    "/rules": "reminds you of the channel rules admin only",
+    "/delete": "delete the message that violates group rules admin only",
 }
 
 rulesArray = [
@@ -49,10 +64,6 @@ rulesArray = [
 ]
 
 
-# app = Flask(__name__)
-
-
-# bot = Bot(token=BOT_TOKEN) # type: ignore
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -63,12 +74,44 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("I will send news every two hours")  # type: ignore
 
 
+bot = Bot(BOT_TOKEN)  # type:ignore
+
+
+# welcomes new members
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.effective_chat.send_action(action=ChatAction.TYPING)  # type: ignore
     await update.message.reply_text("List Of commands Available")  # type: ignore
     string_map = "\n".join([f"{key}: {value}" for key, value in commands.items()])
     await update.message.reply_text(string_map)  # type: ignore
+
+
+def extract_status_change(chat_member_update: ChatMemberUpdated) -> Optional[Tuple[bool, bool]]:
+    """Takes a ChatMemberUpdated instance and extracts whether the 'old_chat_member' was a member
+    of the chat and whether the 'new_chat_member' is a member of the chat. Returns None, if
+    the status didn't change.
+    """
+    status_change = chat_member_update.difference().get("status")
+    old_is_member, new_is_member = chat_member_update.difference().get(
+        "is_member", (None, None)
+    )
+
+    if status_change is None:
+        return None
+
+    old_status, new_status = status_change
+    was_member = old_status in [
+        ChatMember.MEMBER,
+        ChatMember.OWNER,
+        ChatMember.ADMINISTRATOR,
+    ] or (old_status == ChatMember.RESTRICTED and old_is_member is True)
+    is_member = new_status in [
+        ChatMember.MEMBER,
+        ChatMember.OWNER,
+        ChatMember.ADMINISTRATOR,
+    ] or (new_status == ChatMember.RESTRICTED and new_is_member is True)
+
+    return was_member, is_member
 
 
 async def fetch_news_every_2_hrs(context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -78,7 +121,10 @@ async def fetch_news_every_2_hrs(context: ContextTypes.DEFAULT_TYPE) -> None:
     file_name: str = news_content[1]
     photo: str = await image_handler(file_name)
     await context.bot.send_photo(  # type:ignore
-        photo=open(photo, "rb"), caption=text_message, parse_mode="Html",chat_id=context._chat_id  # type:ignore
+        photo=open(photo, "rb"),
+        caption=text_message,
+        parse_mode="Html",
+        chat_id=context._chat_id,  # type:ignore
     )
     os.remove(photo)  # Garbage collector
     news_content.clear()  # Garbage collector
@@ -89,6 +135,35 @@ async def rules(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.effective_chat.send_action(action=ChatAction.TYPING)  # type: ignore
     message = "\n".join(rulesArray)
     await update.message.reply_text(message, parse_mode="Html")  # type: ignore
+
+
+async def greet_chat_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Greets new users in chats and announces when someone leaves"""
+    result = extract_status_change(update.chat_member) #type:ignore
+    if result is None:
+        return
+
+    was_member, is_member = result
+    cause_name = update.chat_member.from_user.mention_html() #type:ignore
+    member_name = update.chat_member.new_chat_member.user.mention_html() #type:ignore
+
+    if not was_member and is_member:
+        await update.effective_chat.send_message( #type:ignore
+            f"{member_name} was added by {cause_name}. Welcome! to The Channel",
+            parse_mode="Html",
+        )
+    elif was_member and not is_member:
+        await update.effective_chat.send_message( #type:ignore
+            f"{member_name} is no longer with us. Thanks a lot, {cause_name} ...",
+            parse_mode="Html",
+        )
+
+
+async def filter_non_admin_messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message
+
+    if not message.from_user.name == ChatMember.ADMINISTRATOR: #type:ignore
+        return True
 
 
 async def news(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -146,6 +221,13 @@ async def get_stockprice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     )  # type:ignore
 
 
+async def ban_users_that_sends_link(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    if filter_non_admin_messages:
+        await update.effective_chat.ban_member(user_id=context._user_id)  # type:ignore
+
+
 def main():
     """Run the bot."""
     # Create the Application and pass it your bot's token.
@@ -159,6 +241,16 @@ def main():
     application.add_handler(CommandHandler("price", get_stockprice))
     application.add_handler(CommandHandler("pricelist", get_priceList))
     application.add_handler(CommandHandler("delete", delete))
+    application.add_handler(
+        MessageHandler(
+            filters.TEXT & (filters.Entity(MessageEntity.URL)),
+            ban_users_that_sends_link,
+        )
+    )
+    application.add_handler(
+        ChatMemberHandler(greet_chat_members, ChatMemberHandler.CHAT_MEMBER)
+    )
+
     # application.add_handler(CallbackQueryHandler(button))
     # runs every two hours
     # Run the bot until the user presses Ctrl-C
